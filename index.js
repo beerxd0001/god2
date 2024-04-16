8e645b34e8e12da67298722fbd71fcccd2674d4d
beerxd0001 <166892344+beerxd0001@users.noreply.github.com>
2024-04-16 23:56:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ client.on('ready', async () => {
     const ddds = ['https://discord.gg/JJztgNj9']; // ลิ้ง button1
 
    const iooi = ['Join Server ']; // ชื่อ button 1
-   const iiio = ['https://discord.gg/JJztgNj9']; // ลิ้ง button 2
-   const tyyy = yyyt[Math.floor(Math.random()*yyyt.length)]; // ชื่อ button 2
+   //const iiio = ['https://discord.gg/JJztgNj9']; // ลิ้ง button 2
+   //const tyyy = yyyt[Math.floor(Math.random()*yyyt.length)]; // ชื่อ button 2
    //const sddd = ddds[Math.floor(Math.random()*ddds.length)]; // ลิ้ง button 2
    const oooi = 
 iiio[Math.floor(Math.random()*iiio.length)]; // button 1 ใส่ลิ้ง
